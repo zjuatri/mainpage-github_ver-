bd0a5359c4993fa6400b2db4905207c06b608b6f
zjuatri <zjuatri@qq.com>
2024-05-12 03:27:46 +0800

    roughly finish blog

--- mainpage/app/components/NavBar.jsx
+++ mainpage/app/components/NavBar.jsx
@@ -6,11 +6,11 @@ export default function NavBar() {
         <div className="NavBody">
             <div className="nav">
                 <ul>
-                    <li><Link href="./">主页</Link></li>
-                    <li><Link href="introduction">我是谁</Link></li>
-                    <li><Link href="tools">工具</Link></li>
-                    <li><Link href="notes">笔记</Link></li>
-                    <li><Link href="friends">友链</Link></li>
+                    <li><Link href="/">主页</Link></li>
+                    <li><Link href="/introduction">我是谁</Link></li>
+                    <li><Link href="/tools">工具</Link></li>
+                    <li><Link href="/blog">博客</Link></li>
+                    <li><Link href="/friends">友链</Link></li>
                     <div className="nav-box"></div>
                 </ul>
             </div>

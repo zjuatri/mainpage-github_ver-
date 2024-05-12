aff0c1e5754e0194871ceeaeb744f7c5b54d9792
zjuatri <zjuatri@qq.com>
2024-05-12 18:55:47 +0800

    fix a bug

--- mainpage/app/blog/special/mothersDay/page.jsx
+++ mainpage/app/blog/special/mothersDay/page.jsx
@@ -3,7 +3,7 @@ import "./mother.css"
 export default function mother(){
     return(
         <div className="body">
-            <div className="container">
+                <div className="container">
             <div className="content">
                 祝我亲爱的妈妈母亲节快乐！
                 <img className="gift" src="/gift.jpg"></img>

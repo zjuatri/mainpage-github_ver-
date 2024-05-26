fb946148757d5dd4e6624c6652b87dafbc75fece
zjuatri <zjuatri@qq.com>
2024-05-27 00:25:04 +0800

    roughly finish tools

--- mainpage/app/layout.jsx
+++ mainpage/app/layout.jsx
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
         </div>
         {children}
         <footer className="footer">
+        <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width={330} height={86} src="//music.163.com/outchain/player?type=2&id=2099209433&auto=1&height=66"></iframe>
           <div className="social_media">
           <Link href={"https://www.zhihu.com/people/wztsxyh"}>
           <Image
--- mainpage/app/tools/page.jsx
+++ mainpage/app/tools/page.jsx
@@ -1,6 +1,211 @@
+import "./style.min.css"
+import "./titlenotes.css"
+import "./zui.min.pure.css"
 export default function tools() {
     return (
-<div className="body"></div>
+
+      <div className="body">
+        <div class="main-index">
+          <div class="container content-box">
+            <div class="row dh">
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat1">浙大 · 教学</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="xzzd" href="https://courses.zju.edu.cn/user/index"
+                      target="_blank">学在浙大</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jww" href="http://zdbk.zju.edu.cn/" target="_blank">教务网(本)</a></div>
+                  <div class="col-xs-4 mb-2"><a id="cls" href="https://chalaoshi.de/" target="_blank">查老师</a></div>
+                  <div class="col-xs-4 mb-2"><a id="tsgyy" href="https://booking.lib.zju.edu.cn/" target="_blank">图书馆预约</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="zykt" href="https://classroom.zju.edu.cn/" target="_blank">智云课堂</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="wsbm" href="http://zdbk.zju.edu.cn/kszx/index/initMenu.zf"
+                      target="_blank">网上报名</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jjw_yjs" href="https://yjsy.zju.edu.cn/" target="_blank">教务网(研)</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="yzzd" href="https://research.zju.edu.cn/" target="_blank">研在浙大</a></div>
+                  <div class="col-xs-4 mb-2"><a id="pta" href="https://pintia.cn/problem-sets?tab=0" target="_blank">PTA</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="sztzw" href="http://sztz.zju.edu.cn/" target="_blank">素质拓展网</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jxzygl"
+                      href="https://zjuers.com/rd?url=http://jxzygl.zju.edu.cn/zypt/teacher/home"
+                      title="需要校园网或RVPN/WebVPN，加载可能较慢。已使用轻首页智能跳转。" target="_blank">教学资源管理</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jsxxcx" href="https://person.zju.edu.cn/" target="_blank">教师信息查询</a>
+                  </div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat2">浙大 · 信息化服务</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="rvpn" href="https://rvpn.zju.edu.cn/" target="_blank">RVPN</a></div>
+                  <div class="col-xs-4 mb-2"><a id="webvpn" href="https://webvpn.zju.edu.cn/" target="_blank">WebVPN</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="swrz" href="http://net3.zju.edu.cn/" title="校内ZJUWLAN上网认证页面"
+                      target="_blank">上网认证</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zbrj" href="http://ms.zju.edu.cn/" target="_blank">正版软件</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zdyx" href="https://zjuem.zju.edu.cn/" target="_blank">浙大邮箱</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wfgl" href="https://myvpn.zju.edu.cn/" target="_blank">网费管理</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zdyp" href="https://pan.zju.edu.cn/zjusso/" target="_blank">浙大云盘</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="zdyq" href="https://yuque.zju.edu.cn/" target="_blank">浙大语雀</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zdbd" href="https://form.zju.edu.cn/" target="_blank">浙大表单</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zdfwpt" href="https://service.zju.edu.cn/" target="_blank">浙大服务平台</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="swfww" href="http://xwfw.zju.edu.cn/" target="_blank">校务服务网</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zhfww" href="http://zhfw.zju.edu.cn/" title="需要校园网或RVPN/WebVPN"
+                      target="_blank">综合服务网</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat3">浙大 · 其它</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="cc98campus" href="https://zjuers.com/rd?url=https://www.cc98.org/"
+                      title="需要校园网或RVPN/WebVPN。已使用轻首页智能跳转。" target="_blank">CC98</a></div>
+                  <div class="col-xs-4 mb-2"><a id="nhd" href="http://www.nexushd.org/" title="需要校园网或RVPN/WebVPN"
+                      target="_blank">NexusHD</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ddxyq" href="https://www.duoduo.link/" target="_blank">朵朵校友圈</a></div>
+                  <div class="col-xs-4 mb-2"><a id="98webvpn" href="http://www-cc98-org-s.webvpn.zju.edu.cn:8001/"
+                      title="通过WebVPN访问，如卡顿请刷新" target="_blank">CC98(校外)</a></div>
+                  <div class="col-xs-4 mb-2"><a id="eta" href="https://zjuers.com/rd?url=http://eta.zju.edu.cn/"
+                      title="需要校园网或RVPN/WebVPN。已使用轻首页智能跳转。" target="_blank">ETA</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bccx"
+                      href="http://www.life.zju.edu.cn/_web/_apps/lightapp/busQuery/mobile/app/pub/index.html"
+                      target="_blank">班车查询</a></div>
+                  <div class="col-xs-4 mb-2"><a id="tzzd" href="https://zjuers.com/rd?url=http://tzzd.zju.edu.cn"
+                      title="需要校园网或RVPN/WebVPN。已使用轻首页智能跳转。" target="_blank">团在浙大</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jcc" href="http://cwcx.zju.edu.cn/WFManager/login.jsp"
+                      target="_blank">计财处</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jyfw" href="http://www.career.zju.edu.cn/" target="_blank">就业服务</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="xydt" href="https://map.zju.edu.cn/" target="_blank">校园地图</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bksy" href="https://zjuers.com/rd?url=https://bksy.zju.edu.cn/"
+                      title="需要校园网或RVPN/WebVPN。已使用轻首页智能跳转。" target="_blank">本科生院</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bgdhcx"
+                      href="https://tel.zju.edu.cn/_web/_customizes/zju/offic_phone/app/home.html"
+                      target="_blank">办公电话查询</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat4">学术搜索 · 查重</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="zdtsg" href="http://libweb.zju.edu.cn/" target="_blank">浙大图书馆</a></div>
+                  <div class="col-xs-4 mb-2"><a id="qsxsss" href="https://zju.summon.serialssolutions.com/"
+                      title="建议校园网或RVPN/WebVPN" target="_blank">求是学术搜索</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zgzw" href="https://www.cnki.net/" target="_blank">中国知网</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ggxs" href="https://scholar.google.com/"
+                      title="大陆访问需要VPN，也可尝试上方搜索框的学术镜/学术2进行搜索" target="_blank">谷歌学术</a></div>
+                  <div class="col-xs-4 mb-2"><a id="byxs" href="https://cn.bing.com/academic" target="_blank">必应学术</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bdxs" href="https://xueshu.baidu.com/" target="_blank">百度学术</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wpw" href="http://www.cqvip.com/" target="_blank">维普网</a></div>
+                  <div class="col-xs-4 mb-2"><a id="axs" href="https://www.ixueshu.com/" target="_blank">爱学术</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wfsj" href="https://www.wanfangdata.com.cn/index.html"
+                      target="_blank">万方数据</a></div>
+                  <div class="col-xs-4 mb-2"><a id="github" href="https://github.com/" title="网络不佳时请考虑使用代理或镜像"
+                      target="_blank">Github</a></div>
+                  <div class="col-xs-4 mb-2"><a id="webofscience" href="https://www.webofscience.com/"
+                      title="建议校园网或RVPN/WebVPN" target="_blank">WOS</a></div>
+                  <div class="col-xs-4 mb-2"><a id="scihub" href="https://sci-hub.ru/" target="_blank">Sci-Hub</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat5">实用工具</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="ygmh" href="https://ol.woobx.cn/" target="_blank">一个木函</a></div>
+                  <div class="col-xs-4 mb-2"><a id="qqtool" href="https://tool.browser.qq.com/" target="_blank">在线工具箱</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="tgxz" href="http://www.xmsj.org/" target="_blank">听歌下载</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bdfy" href="https://fanyi.baidu.com/" target="_blank">百度翻译</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ggfy" href="https://translate.google.com/" title="无法访问时请考虑使用代理"
+                      target="_blank">谷歌翻译</a></div>
+                  <div class="col-xs-4 mb-2"><a id="slcfy" href="https://lab.magiconch.com/nbnhhsh/"
+                      target="_blank">缩略词复原</a></div>
+                  <div class="col-xs-4 mb-2"><a id="dwz" href="https://sina.lt/" target="_blank">短网址工具</a></div>
+                  <div class="col-xs-4 mb-2"><a id="QRcodetool" href="https://cli.im/text" target="_blank">二维码生成器</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wjkt" href="https://airportal.cn/" target="_blank">文件空投</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wjgszh" href="https://convertio.co/zh/" target="_blank">文件格式转换</a></div>
+                  <div class="col-xs-4 mb-2"><a id="pdftool" href="https://www.ilovepdf.com/zh-cn"
+                      target="_blank">PDF工具箱</a></div>
+                  <div class="col-xs-4 mb-2"><a id="imgedit" href="https://www.iloveimg.com/zh-cn"
+                      target="_blank">在线图片编辑</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat6">学习 · 影视</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="mooc" href="https://www.icourse163.org/" target="_blank">MOOC</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zhs" href="https://www.zhihuishu.com/" target="_blank">智慧树</a></div>
+                  <div class="col-xs-4 mb-2"><a id="xxqg" href="https://www.xuexi.cn/" target="_blank">学习强国</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bilibili" href="https://www.bilibili.com/" target="_blank">Bilibili</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="aqy" href="https://www.iqiyi.com/" target="_blank">爱奇艺</a></div>
+                  <div class="col-xs-4 mb-2"><a id="txsp" href="https://v.qq.com/" target="_blank">腾讯视频</a></div>
+                  <div class="col-xs-4 mb-2"><a id="yk" href="https://www.youku.com/" target="_blank">优酷</a></div>
+                  <div class="col-xs-4 mb-2"><a id="mgtv" href="https://www.mgtv.com/" target="_blank">芒果TV</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ysw" href="https://tv.cctv.com/" target="_blank">CCTV</a></div>
+                  <div class="col-xs-4 mb-2"><a id="yk" href="https://www.huya.com/" target="_blank">虎牙直播</a></div>
+                  <div class="col-xs-4 mb-2"><a id="mgtv" href="https://www.douyu.com/" target="_blank">斗鱼直播</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ysw" href="https://live.qq.com/" target="_blank">企鹅体育</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat7">实用工具</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="txwd" href="https://docs.qq.com/desktop/" target="_blank">腾讯文档</a></div>
+                  <div class="col-xs-4 mb-2"><a id="bdwk" href="https://wenku.baidu.com/" target="_blank">百度文库</a></div>
+                  <div class="col-xs-4 mb-2"><a id="xiumi" href="https://xiumi.us/" target="_blank">秀米</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wolfram" href="https://www.wolframalpha.com/"
+                      target="_blank">Wolfram</a></div>
+                  <div class="col-xs-4 mb-2"><a id="myscript" href="https://webdemo.myscript.com/"
+                      target="_blank">Myscript</a></div>
+                  <div class="col-xs-4 mb-2"><a id="geogebra" href="https://www.geogebra.org/" target="_blank">Geogebra</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="wjx" href="https://www.wjx.cn/" target="_blank">问卷星</a></div>
+                  <div class="col-xs-4 mb-2"><a id="flvdl" href="https://bilibili.iiilab.com/" target="_blank">视频下载</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="zzgj" href="https://tool.chinaz.com/" target="_blank">站长工具</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat8">社交 · 娱乐</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="zhihu" href="https://www.zhihu.com/hot" target="_blank">知乎</a></div>
+                  <div class="col-xs-4 mb-2"><a id="db" href="https://www.douban.com/" target="_blank">豆瓣</a></div>
+                  <div class="col-xs-4 mb-2"><a id="nga" href="https://bbs.nga.cn/" target="_blank">NGA</a></div>
+                  <div class="col-xs-4 mb-2"><a id="wbrs" href="https://s.weibo.com/top/summary" target="_blank">微博热搜</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="txty" href="https://sports.qq.com/" target="_blank">腾讯体育</a></div>
+                  <div class="col-xs-4 mb-2"><a id="hpw" href="https://www.hupu.com/" target="_blank">虎扑网</a></div>
+                  <div class="col-xs-4 mb-2"><a id="Pixivic" href="https://pixivic.com/" target="_blank">Pixivic</a></div>
+                  <div class="col-xs-4 mb-2"><a id="jd" href="https://www.jd.com/" target="_blank">京东商城</a></div>
+                  <div class="col-xs-4 mb-2"><a id="taobao" href="https://www.taobao.com/" target="_blank">淘宝网</a></div>
+                </div>
+              </div>
+              <div class="col-sm-6 col-md-4 mb-3 dh">
+                <div class="row">
+                  <div class="col-xs-12"><strong class="dhname" id="cat9">其它 · 关于本站</strong></div>
+                  <div class="col-xs-4 mb-2"><a id="wlcs" href="https://www.speedtest.cn/" target="_blank">网络测速</a></div>
+                  <div class="col-xs-4 mb-2"><a id="sshb" href="https://zh.flightaware.com/live/" target="_blank">实时航班</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="zxbdsm" href="https://www.virscan.org/" target="_blank">在线病毒扫描</a></div>
+                  <div class="col-xs-4 mb-2"><a id="zdxl"
+                      href="https://zjuers.com/banner/%E6%B5%99%E6%B1%9F%E5%A4%A7%E5%AD%A62023-2024%E5%AD%A6%E5%B9%B4%E6%A0%A1%E5%8E%86.pdf"
+                      target="_blank">浙大校历</a></div>
+                  <div class="col-xs-4 mb-2"><a id="about" href="http://speedtest.zju.edu.cn/" target="_blank">校网测速</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="ssyqsm" href="https://zhfw.zju.edu.cn/65686/list.htm"
+                      title="需要校园网或RVPN/WebVPN" target="_blank">浙大黄页</a></div>
+                  <div class="col-xs-4 mb-2"><a id="ssyqsm" href="https://www.cc98.org/topic/4979979"
+                      title="需要校园网或RVPN/WebVPN" target="_blank">搜索引擎说明</a></div>
+                  <div class="col-xs-4 mb-2"><a id="about" href="https://zjuers.com/about" target="_blank">关于本站&amp;反馈</a>
+                  </div>
+                  <div class="col-xs-4 mb-2"><a id="blackmode" href="https://zjuers.com/black" target="_self"># 深色模式 #</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
   
